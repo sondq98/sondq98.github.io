0ce11896fbde09f219032dea4637a7e8688f21cb
Sondq <sondq98@gmail.com>
2021-04-12 10:55:21 +0700

    Push layout quiz app

---
+++ MiniQuiz/app.js
@@ -0,0 +1,36 @@
+const quizData = [
+    {
+        question: 'Which is correct answer',
+        a: 'This answer',
+        b: 'Not this',
+        c: 'Not this',
+        d: 'Not this',
+        correct: 'a'
+    },
+    {
+        question: 'Question-1',
+        a: 'Not this',
+        b: 'This answer',
+        c: 'Not this',
+        d: 'Not this',
+        correct: 'b'
+    },
+    {
+        question: 'Question-1',
+        a: 'Not this',
+        b: 'Not this',
+        c: 'This answer',
+        d: 'Not this',
+        correct: 'c'
+    },
+    {
+        question: 'Question-1',
+        a: 'Not this',
+        b: 'Not this',
+        c: 'Not this',
+        d: 'This answer',
+        correct: 'd'
+    },
+]
+
+const time = new Date()
