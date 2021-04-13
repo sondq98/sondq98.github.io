8931d432d9f69ad84bd55fe6ddb5b44a9d2f5518
Sondq <sondq98@gmail.com>
2021-04-13 19:12:04 +0700

    Finish js for quiz

--- MiniQuiz/app.js
+++ MiniQuiz/app.js
@@ -33,28 +33,53 @@ const quizData = [
     },
 ]
 
-const questionQuiz = document.querySelector('.ques>h2');
+var score = 0;
 
+const questionQuiz = document.querySelector('.ques>h2');
+const ansDiv = document.querySelector('.ans');
 const a_ans = document.querySelector('#a_text');
-console.log(a_ans);
 const b_ans = document.querySelector('#b_text');
 const c_ans = document.querySelector('#c_text');
 const d_ans = document.querySelector('#d_text');
 
 let currentQuiz = 0;
-
 function loadQuiz() {
     const currentQuizData = quizData[currentQuiz];
-
     questionQuiz.innerText = currentQuizData.question;
     a_ans.innerText = currentQuizData.a;
     b_ans.innerText = currentQuizData.b;
     c_ans.innerText = currentQuizData.c;
     d_ans.innerText = currentQuizData.d;
-
-    currentQuiz++;
 }
 
-loadQuiz();
+function getSelected() {
+    const answers = document.querySelectorAll('.answer');
+    
+    let result = undefined;
+    answers.forEach(ans => {
+        if (ans.checked) {
+            result = ans.value;
+        }
+    })
+    return result;
+}
+
+
 const submitBtn = document.querySelector('.submit');
-submitBtn.addEventListener("click", loadQuiz);
+submitBtn.addEventListener("click", () => {
+    if (getSelected() == quizData[currentQuiz].correct) {
+        score++;
+    }
+
+    currentQuiz++;
+    if (currentQuiz < quizData.length) {
+        loadQuiz();
+    } else {
+        questionQuiz.innerText = 'You has finishes';
+        const scoreDirect = document.querySelector('.ans');
+        scoreDirect.innerHTML = `<h1>Your score is ${score}/${quizData.length}</h1>`;
+        scoreDirect.style.textAlign = 'center';
+    }
+});
+
+loadQuiz();
